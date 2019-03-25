fbff75f2372428ff3303059c72e29b9e64d77e31
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-03-25 17:49:58 +0100

    init repo oxidio/module-pim

---
+++ metadata.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright (C) oxidio. See LICENSE file for license details.
+ */
+
+$sMetadataVersion = '2.1';
+$aModule          = Oxidio\Module\Module::instance(fn\VENDOR\OXIDIO\MODULE_PIM)->getMetadata();
---
+++ src/autoload-files.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Copyright (C) oxidio. See LICENSE file for license details.
+ */
+
+require_once __DIR__ . '/constants.php';
